44f447213b7c213549913f99af534f6a6078fee9
Di-Esme <unkluzer186@gmail.com>
2024-01-21 23:16:13 +0700

    add push changes 2

---
+++ push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `27dd938f7de5538187e190fed101e3b6`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
